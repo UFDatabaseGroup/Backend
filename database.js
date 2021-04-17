96537c170eb503c6b876272d87318c68bd1558f4
iczero <iczero4@gmail.com>
2021-04-17 16:39:27 -0700

    replace trend query 5

--- database.js
+++ database.js
@@ -165,10 +165,11 @@ async function trendQuery4(country) {
 
 async function trendQuery5() {
     return await query(`
-        select country, timestamp_id, (avg(case_fatality_ratio) / sum(active)) as derp_rate from "J.LUO".COVID_DATA
-        where active > 0 and case_fatality_ratio is not null
-        group by country, timestamp_id
-        order by timestamp_id asc
+        select (Deaths_Country / Deaths_Worldwide) * 100 as Deaths_Contributed, Deaths_Country, Deaths_Worldwide, worldTime
+        from
+            (select SUM(Deaths) as Deaths_Worldwide, TIMESTAMP_ID as worldTime from "J.LUO".COVID_DATA where deaths is not null group by timestamp_id) worldData,
+            (select SUM(Deaths) as Deaths_Country, TIMESTAMP_ID as countryTime, Country from "J.LUO".COVID_DATA where country = 'United States' and deaths is not null group by TIMESTAMP_ID, Country) countryData
+        where worldTime = countryTime order by worldTime;
     `);
 }
 
